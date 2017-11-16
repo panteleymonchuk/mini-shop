b0664776f30e4e41601fc26df055c3d95dbbc969
E.Panteleymonchuk <e.panteleymonchuk@nullgr.com>
2017-11-16 18:43:38 +0200

    Added redux modules

---
+++ src/features/App/reducers/index.js
@@ -0,0 +1,7 @@
+import app from './app';
+
+const AppReducers = {
+  app,
+};
+
+export default AppReducers;
---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import persistState from 'redux-localstorage';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// reducers imports
+import AppReducers from 'src/features/App/reducers';
+
+const createLocalStore = compose(
+  persistState(['cachedLS'], {
+    key: ''
+  })
+)(createStore);
+
+const mergedReducers = {
+  ...AppReducers,
+};
+
+let store = createLocalStore(combineReducers(mergedReducers), {}, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
