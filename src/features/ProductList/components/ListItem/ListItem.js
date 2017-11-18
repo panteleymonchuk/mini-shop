1533136baae4609057ec7f2e639ad757f5f94e22
E.Panteleymonchuk <e.panteleymonchuk@nullgr.com>
2017-11-18 16:40:57 +0200

    ListItem done

---
+++ src/features/ProductList/components/ListItem/ListItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import listItem from './../../../../images/listItem.png';
+
+import './ListItem.css';
+
+const ListItem = ({ name, price }) => {
+  return (
+    <div className="listItem">
+      <img className="listItem__img" src={listItem} alt=""/>
+      <h4 className="listItem__name">{name}</h4>
+      <p className="listItem__price">{price} USD</p>
+      <button className='listItem__addToCart'>Add to cart</button>
+    </div>
+  );
+};
+
+const propTypes = {
+  name: PropTypes.string
+};
+const defaultProps = {};
+
+ListItem.propTypes = propTypes;
+ListItem.defaultProps = defaultProps;
+
+export default ListItem;
--- src/features/ProductList/components/ProductList.js
+++ src/features/ProductList/components/ProductList.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import ListItem from './ListItem/ListItem';
+
 import './ProductList.css';
 
 class ProductList extends Component {
   render() {
 
-    // const ListItem =
+    const { productList } = this.props;
+
+    const products = productList.map((listItem) => <ListItem key={listItem.id} {...listItem}/>);
 
     return (
-      <div className="ProductList">
-        Componentffasfs
+      <div className="productList">
+        <h3 className="productList__header">Products</h3>
+        <div className="productList__products">
+          {products}
+        </div>
       </div>
     );
   }
 }
 
-export default ProductList;
+ProductList.propTypes = {
+  productList: PropTypes.array
+};
+
+const mapStateToProps = ({ productList }) => ({
+  productList
+});
+
+export default connect(mapStateToProps, null)(ProductList);
